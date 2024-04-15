17394068cf3268d5769a0d3c6a1d1bbb1905c724
kyb <kyb-0215@naver.com>
2024-04-15 19:38:43 +0900

    DI 의존성 주입

---
+++ src/main/java/com/springblog/component/Zoo.java
@@ -0,0 +1,51 @@
+package com.springblog.component;
+
+import com.springblog.domain.Animal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Zoo {
+
+//     직접
+//    Animal animal = new Animal();
+
+    private final Animal animal;
+
+    // 생성자 주입
+    @Autowired
+    public Zoo(Animal animal) {
+        this.animal = animal;
+    }
+
+
+//    //    setter 주입
+//    private Animal animal;
+//
+//    @Autowired
+//    public void setAnimal(Animal animal) {
+//        this.animal = animal;
+//    }
+
+
+}
+
+class ZooTest {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("호랑이", 13);
+        Zoo zoo = new Zoo(animal);
+
+
+//        // 먼저 Zoo 객체 생성
+//        // Animal 객체 생성 후 Setter 메서드를 통해 주입
+//        Zoo zoo = new Zoo();
+//        Animal animal = new Animal();
+//        animal.setName("코끼리");
+//        animal.setAge(10);
+//        zoo.setAnimal(animal);
+
+
+    }
+}
+
---
+++ src/main/java/com/springblog/domain/Animal.java
@@ -0,0 +1,36 @@
+package com.springblog.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Animal {
+    private String name;
+    private int age;
+
+    // 생성자
+    public Animal() {
+    }
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getter와 setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}

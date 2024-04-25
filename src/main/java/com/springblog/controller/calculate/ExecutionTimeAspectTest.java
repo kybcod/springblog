688f720f21221b4af6ed7d1ced12699ed6a39e3f
kyb <kyb-0215@naver.com>
2024-04-25 09:40:33 +0900

    mybatis setting

--- src/main/java/com/springblog/controller/calculate/ExecutionTimeAspectTest.java
+++ src/main/java/com/springblog/controller/calculate/ExecutionTimeAspectTest.java
@@ -1,29 +1,29 @@
-package com.springblog.controller.calculate;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class ExecutionTimeAspectTest {
-
-    @Autowired
-    private Calculator basicCalculator;
-
-    @Autowired
-    private Calculator recurCalculator;
-
-    @Test
-    public void test(){
-        long factorialAnswer = basicCalculator.factorial(5);
-        System.out.printf("factorial 결과 : %d \n", factorialAnswer);
-        System.out.printf("실행된 객체 : %s \n", basicCalculator.getClass().getName());
-
-        System.out.println("=======================================");
-
-        long factorialAnswer2 = recurCalculator.factorial(5);
-        System.out.printf("factorial 결과 : %d \n", factorialAnswer2);
-        System.out.printf("실행된 객체 : %s \n", recurCalculator.getClass().getName());
-
-    }
-}
+//package com.springblog.controller.calculate;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class ExecutionTimeAspectTest {
+//
+//    @Autowired
+//    private Calculator basicCalculator;
+//
+//    @Autowired
+//    private Calculator recurCalculator;
+//
+//    @Test
+//    public void test(){
+//        long factorialAnswer = basicCalculator.factorial(5);
+//        System.out.printf("factorial 결과 : %d \n", factorialAnswer);
+//        System.out.printf("실행된 객체 : %s \n", basicCalculator.getClass().getName());
+//
+//        System.out.println("=======================================");
+//
+//        long factorialAnswer2 = recurCalculator.factorial(5);
+//        System.out.printf("factorial 결과 : %d \n", factorialAnswer2);
+//        System.out.printf("실행된 객체 : %s \n", recurCalculator.getClass().getName());
+//
+//    }
+//}

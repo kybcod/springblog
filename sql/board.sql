62688dbdf2c257b04e3e8aa75b879c582a8358cc
ybk <kyb-0215@naver.com>
2024-05-06 02:44:32 +0900

    board 테이블 생성

--- sql/board.sql
+++ sql/board.sql
@@ -13,3 +13,15 @@ CREATE TABLE member
 
 select *
 FROM member;
+
+CREATE TABLE board
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(200)  NOT NULL,
+    content  VARCHAR(3000) NOT NULL,
+    writer   VARCHAR(100)  NOT NULL,
+    inserted DATETIME      NOT NULL DEFAULT NOW()
+);
+
+SELECT *
+FROM board;
